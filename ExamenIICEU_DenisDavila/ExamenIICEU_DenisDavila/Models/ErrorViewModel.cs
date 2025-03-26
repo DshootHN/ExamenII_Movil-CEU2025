0b4c70ff462af9e2fe09fd350286c5e8bbb532fc
Denis Dávila <denisdavilask04@gmail.com>
2025-03-26 17:59:29 -0600

    Crear Proyecto

---
+++ ExamenIICEU_DenisDavila/ExamenIICEU_DenisDavila/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamenIICEU_DenisDavila.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
